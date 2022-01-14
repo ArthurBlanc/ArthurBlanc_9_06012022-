b1514477f70e22ce28fb1656079419ca78a733fb
Arthur BLANC <arthur.blanc@outlook.com>
2022-01-14 15:56:58 +0100

    fix bill order (descendent) in employee dashboard

--- Front-end/src/views/BillsUI.js
+++ Front-end/src/views/BillsUI.js
@@ -20,6 +20,8 @@ const row = (bill) => {
 };
 
 const rows = (data) => {
+	// fix bill order : add descendent sorting
+	data.sort((a, b) => (new Date(a.date) < new Date(b.date) ? 1 : -1));
 	return data && data.length ? data.map((bill) => row(bill)).join("") : "";
 };
 
