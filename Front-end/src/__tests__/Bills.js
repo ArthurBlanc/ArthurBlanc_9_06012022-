6bc5adc9f971dd6a9b95f76c4987a53e2f9b3022
Arthur BLANC <arthur.blanc@outlook.com>
2022-01-28 17:44:07 +0100

    add more tests for Bills.js

--- Front-end/src/__tests__/Bills.js
+++ Front-end/src/__tests__/Bills.js
@@ -1,47 +1,125 @@
 import { screen } from "@testing-library/dom";
 import BillsUI from "../views/BillsUI.js";
-import { bills } from "../fixtures/bills.js";
+import { localStorageMock } from "../__mocks__/localStorage.js";
 import store from "../__mocks__/store";
+import { bills } from "../fixtures/bills";
 
-describe("Given I am connected as an employee", () => {
+describe("Given I am connected as an Employee", () => {
 	// Test for billsUI.js
-	describe("When I am on Bills Page", () => {
-		test("Then bill icon in vertical layout should be highlighted", () => {
+	describe("When I am on Bills page, there are a bill icon in vertical layout", () => {
+		test("Then, the icon should be highlighted", () => {
+			Object.defineProperty(window, "localStorage", { value: localStorageMock });
 			window.localStorage.setItem(
 				"user",
 				JSON.stringify({
 					type: "Employee",
 				})
 			);
+
 			const html = BillsUI({ data: [] });
 			document.body.innerHTML = html;
+
 			expect(document.querySelector("#layout-icon1").classList.contains("active-icon"));
 		});
-		test("Then bills should be ordered from earliest to latest", () => {
-			const html = BillsUI({ data: bills });
-			document.body.innerHTML = html;
-			const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map((a) => a.innerHTML);
-			const antiChrono = (a, b) => (a < b ? 1 : -1);
-			const datesSorted = [...dates].sort(antiChrono);
-			expect(dates).toEqual(datesSorted);
+		describe("When I am on Bills page, there are a title and a newBill button", () => {
+			test("Then, the title and the button should be render correctly", () => {
+				const html = BillsUI({ data: [] });
+				document.body.innerHTML = html;
+
+				expect(screen.getAllByText("Mes notes de frais")).toBeTruthy();
+				expect(screen.getByTestId("btn-new-bill")).toBeTruthy();
+			});
+		});
+		describe("When I am on Bills page, there are 4 bills", () => {
+			test("Then, bills data should be render 4 type, name, date, amount, status and eye icon", () => {
+				const html = BillsUI({ data: bills });
+				document.body.innerHTML = html;
+
+				const bill = screen.getAllByTestId("bill");
+				const type = screen.getAllByTestId("type");
+				const name = screen.getAllByTestId("name");
+				const date = screen.getAllByTestId("date");
+				const amount = screen.getAllByTestId("amount");
+				const status = screen.getAllByTestId("status");
+				const iconEye = screen.getAllByTestId("icon-eye");
+
+				expect(bill.length).toBe(4);
+				expect(type.length).toBe(4);
+				expect(name.length).toBe(4);
+				expect(date.length).toBe(4);
+				expect(amount.length).toBe(4);
+				expect(status.length).toBe(4);
+				expect(iconEye.length).toBe(4);
+			});
+		});
+		describe("When I am on Bills page, there are bills", () => {
+			test("Then, first bill data should contain the right type, name, date, amount, status and eye icon", () => {
+				const html = BillsUI({ data: bills });
+				document.body.innerHTML = html;
+
+				const bill = screen.getAllByTestId("bill");
+				const type = screen.getAllByTestId("type")[0];
+				const name = screen.getAllByTestId("name")[0];
+				const date = screen.getAllByTestId("date")[0];
+				const amount = screen.getAllByTestId("amount")[0];
+				const status = screen.getAllByTestId("status")[0];
+				const iconEye = screen.getAllByTestId("icon-eye")[0];
+
+				expect(bill.length).toBe(4);
+				expect(type.textContent).toBe("Hôtel et logement");
+				expect(name.textContent).toBe("encore");
+				expect(date.textContent).toBe("2004-04-04");
+				expect(amount.textContent).toBe("400 €");
+				expect(status.textContent).toBe("pending");
+				expect(iconEye.textContent).toBeTruthy();
+			});
+		});
+		describe("When I am on Bills page, there are 4 bills", () => {
+			test("Then, bills should be ordered from earliest to latest", () => {
+				const html = BillsUI({ data: bills });
+				document.body.innerHTML = html;
+
+				const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map((a) => a.innerHTML);
+				const antiChrono = (a, b) => (a < b ? 1 : -1);
+				const datesSorted = [...dates].sort(antiChrono);
+
+				expect(dates).toEqual(datesSorted);
+			});
+		});
+		describe("When I am on Bills page, but it is loading", () => {
+			test("Then, Loading page should be rendered", () => {
+				const html = BillsUI({ loading: true });
+				document.body.innerHTML = html;
+				expect(screen.getAllByText("Loading...")).toBeTruthy();
+			});
+		});
+		describe("When I am on Dashboard page but back-end send an error message", () => {
+			test("Then, Error page should be rendered", () => {
+				const html = BillsUI({ error: "some error message" });
+				document.body.innerHTML = html;
+				expect(screen.getAllByText("Erreur")).toBeTruthy();
+			});
 		});
 	});
-	// Test d'intégration GET
+});
+
+// Test d'intégration GET
+describe("Given I am a user connected as Employee", () => {
 	describe("When I navigate to Bill", () => {
-		test("fetches bills from mock API GET", async () => {
+		test("Then, fetches bills from mock API GET", async () => {
 			const getSpy = jest.spyOn(store, "get");
 			const bills = await store.get();
 			expect(getSpy).toHaveBeenCalledTimes(1);
 			expect(bills.data.length).toBe(4);
 		});
-		test("fetches bills from an API and fails with 404 message error", async () => {
+		test("Then, fetches bills from an API and fails with 404 message error", async () => {
 			store.get.mockImplementationOnce(() => Promise.reject(new Error("Erreur 404")));
 			const html = BillsUI({ error: "Erreur 404" });
 			document.body.innerHTML = html;
 			const message = await screen.getByText(/Erreur 404/);
 			expect(message).toBeTruthy();
 		});
-		test("fetches messages from an API and fails with 500 message error", async () => {
+		test("Then, fetches messages from an API and fails with 500 message error", async () => {
 			store.get.mockImplementationOnce(() => Promise.reject(new Error("Erreur 500")));
 			const html = BillsUI({ error: "Erreur 500" });
 			document.body.innerHTML = html;
